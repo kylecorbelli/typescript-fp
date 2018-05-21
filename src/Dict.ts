f318d5433fb272fdf89700d33b4a0c204383309c
Kyle Corbelli <kyle.corbelli@gmail.com>
2018-05-20 18:52:40 -0700

    Get Dict functions mostly curried

--- src/Dict.ts
+++ src/Dict.ts
@@ -1,5 +1,7 @@
 import { Maybe } from './Maybe'
 
+// i can probably write a pretty mean curry2 type and function
+
 export interface HashMap<T> {
   [key: string]: T
 }
@@ -9,61 +11,56 @@ export class Dict<T> {
     return new Dict({})
   }
 
-  public static singleton<T> (key: string, value: T): Dict<T> { // curry it
-    return new Dict({
-      [key]: value,
-    })
+  public static singleton<T> (key: string, value: T): Dict<T>
+  public static singleton<T> (key: string): (value: T) => Dict<T>
+  public static singleton<T> (key: string, value?: T) {
+    return value === undefined
+      ? (v: T): Dict<T> => this.singletonHelper(key, v)
+      : this.singletonHelper(key, value)
   }
 
-  public static get<T> (key: string, dict: Dict<T>): Maybe<T> { // curry it
-    return Maybe.fromNullable(dict._store[key])
-  }
-
-  // it’s questionable whether or not we actually need this:
-  public static fromList<T> (list: ReadonlyArray<[ string, T ]>): Dict<T> {
-    const store: HashMap<T> = list.reduce(
-      (cumulativeStore: HashMap<T>, [ key, val ]: [ string, T ]) => ({
-        ...cumulativeStore,
-        [key]: val,
-      }),
-      {},
-    )
-    return new Dict(store)
+  public static get<T> (key: string, dict: Dict<T>): Maybe<T>
+  public static get<T> (key: string): (dict: Dict<T>) => Maybe<T>
+  public static get<T> (key: string, dict?: Dict<T>) {
+    return dict === undefined
+      ? (d: Dict<T>): Maybe<T> => this.getHelper(key, d)
+      : this.getHelper(key, dict)
   }
 
   public static fromHashMap<T> (hashMap: HashMap<T>): Dict<T> {
     return new Dict(hashMap)
   }
 
-  public static insert<T> (key: string, val: T, dict: Dict<T>): Dict<T> { // curry it
-    const store: HashMap<T> = dict._store
-    const newStore: HashMap<T> = {
-      ...store,
-      [key]: val,
-    }
-    return new Dict(newStore)
+  public static insert<T> (key: string, val: T, dict: Dict<T>): Dict<T>
+  public static insert<T> (key: string, val: T): (dict: Dict<T>) => Dict<T>
+  public static insert<T> (key: string, val: T, dict?: Dict<T>) {
+    return dict === undefined
+      ? (d: Dict<T>): Dict<T> => this.insertHelper(key, val, d)
+      : this.insertHelper(key, val, dict)
   }
 
-  public static member<T> (key: string, dict: Dict<T>): boolean { // curry it
-    return key in dict._store
+  public static member<T> (key: string, dict: Dict<T>): boolean
+  public static member<T> (key: string): (dict: Dict<T>) => boolean
+  public static member<T> (key: string, dict?: Dict<T>) {
+    return dict === undefined
+      ? (d: Dict<T>): boolean => this.memberHelper(key, d)
+      : this.memberHelper(key, dict)
   }
 
-  public static update<T> (key: string, transform: (val: T) => T, dict: Dict<T>): Dict<T> { // curry it
-    if (!this.member(key, dict)) return dict
-    const store: HashMap<T> = dict._store
-    const newStore: HashMap<T> = {
-      ...store,
-      [key]: transform(store[key]),
-    }
-    return new Dict(newStore)
+  public static update<T> (key: string, transform: (val: T) => T, dict: Dict<T>): Dict<T>
+  public static update<T> (key: string, transform: (val: T) => T): (dict: Dict<T>) => Dict<T>
+  public static update<T> (key: string, transform: (val: T) => T, dict?: Dict<T>) {
+    return dict === undefined
+      ? (d: Dict<T>): Dict<T> => this.updateHelper(key, transform, d)
+      : this.updateHelper(key, transform, dict)
   }
 
-  public static remove<T> (key: string, dict: Dict<T>): Dict<T> { // curry it
-    const {
-      [key]: _,
-      ...rest,
-    } = dict._store
-    return new Dict(rest)
+  public static remove<T> (key: string, dict: Dict<T>): Dict<T>
+  public static remove<T> (key: string): (dict: Dict<T>) => Dict<T>
+  public static remove<T> (key: string, dict?: Dict<T>) {
+    return dict === undefined
+      ? (d: Dict<T>): Dict<T> => this.removeHelper(key, d)
+      : this.removeHelper(key, dict)
   }
 
   public static isEmpty<T> (dic: Dict<T>): boolean {
@@ -86,7 +83,61 @@ export class Dict<T> {
     return { ...dict._store }
   }
 
-  public static map<A, B> (f: (key: string, val: A) => B, dict: Dict<A>): Dict<B> { // curry it
+  public static map<A, B> (f: (key: string, val: A) => B, dict: Dict<A>): Dict<B>
+  public static map<A, B> (f: (key: string, val: A) => B): (dict: Dict<A>) => Dict<B>
+  public static map<A, B> (f: (key: string, val: A) => B, dict?: Dict<A>) {
+    return dict === undefined
+      ? (d: Dict<A>): Dict<B> => this.mapHelper(f, d)
+      : this.mapHelper(f, dict)
+  }
+
+  public static filter<T> (predicate: (key: string, val: T) => boolean, dict: Dict<T>): Dict<T>
+  public static filter<T> (predicate: (key: string, val: T) => boolean): (dict: Dict<T>) => Dict<T>
+  public static filter<T> (predicate: (key: string, val: T) => boolean, dict?: Dict<T>) {
+    return dict === undefined
+      ? (d: Dict<T>): Dict<T> => this.filterHelper(predicate, d)
+      : this.filterHelper(predicate, dict)
+  }
+
+  public static reduce<A, B> (f: (key: string, val: A, accum: B) => B, initial: B, dict: Dict<A>): B
+  public static reduce<A, B> (f: (key: string, val: A, accum: B) => B, initial: B): (dict: Dict<A>) => B
+  public static reduce<A, B> (f: (key: string, val: A, accum: B) => B, initial: B, dict?: Dict<A>) {
+    return dict === undefined
+      ? (d: Dict<A>): B => this.reduceHelper(f, initial, d)
+      : this.reduceHelper(f, initial, dict)
+  }
+
+  public static union<T> (first: Dict<T>, second: Dict<T>): Dict<T>
+  public static union<T> (first: Dict<T>): (second: Dict<T>) => Dict<T>
+  public static union<T> (first: Dict<T>, second?: Dict<T>) {
+    return second === undefined
+      ? (s: Dict<T>): Dict<T> => this.unionHelper(first, s)
+      : this.unionHelper(first, second)
+  }
+
+  private static memberHelper<T> (key: string, dict: Dict<T>): boolean {
+    return key in dict._store
+  }
+
+  private static getHelper<T> (key: string, dict: Dict<T>): Maybe<T> {
+    return Maybe.fromNullable(dict._store[key])
+  }
+
+  private static singletonHelper<T> (key: string, value: T): Dict<T> {
+    return new Dict({
+      [key]: value,
+    })
+  }
+
+  private static removeHelper<T> (key: string, dict: Dict<T>): Dict<T> {
+    const {
+      [key]: _,
+      ...rest,
+    } = dict._store
+    return new Dict(rest)
+  }
+
+  private static mapHelper<A, B> (f: (key: string, val: A) => B, dict: Dict<A>): Dict<B> {
     const store = dict._store
     const newStore: HashMap<B> = Object.keys(store).reduce(
       (cumulativeStore: HashMap<B>, key: string) => ({
@@ -98,7 +149,7 @@ export class Dict<T> {
     return new Dict(newStore)
   }
 
-  public static filter<T> (predicate: (key: string, val: T) => boolean, dict: Dict<T>): Dict<T> { // curry it
+  private static filterHelper<T> (predicate: (key: string, val: T) => boolean, dict: Dict<T>): Dict<T> {
     const store = dict._store
     const newStore: HashMap<T> = Object.keys(store).reduce(
       (cumulativeStore: HashMap<T>, key: string) => {
@@ -114,17 +165,36 @@ export class Dict<T> {
     return new Dict(newStore)
   }
 
-  public static reduce<A, B> (f: (key: string, val: A, accum: B) => B, initial: B, dict: Dict<A>): B { // curry it
+  private static unionHelper<T> (first: Dict<T>, second: Dict<T>): Dict<T> {
+    const newStore: HashMap<T> = { ...second._store, ...first._store }
+    return new Dict(newStore)
+  }
+
+  private static insertHelper<T> (key: string, val: T, dict: Dict<T>): Dict<T> {
+    const store: HashMap<T> = dict._store
+    const newStore: HashMap<T> = {
+      ...store,
+      [key]: val,
+    }
+    return new Dict(newStore)
+  }
+
+  private static updateHelper<T> (key: string, transform: (val: T) => T, dict: Dict<T>): Dict<T> {
+    if (!this.member(key, dict)) return dict
+    const store: HashMap<T> = dict._store
+    const newStore: HashMap<T> = {
+      ...store,
+      [key]: transform(store[key]),
+    }
+    return new Dict(newStore)
+  }
+
+  private static reduceHelper<A, B> (f: (key: string, val: A, accum: B) => B, initial: B, dict: Dict<A>): B {
     const store = dict._store
     return Object.keys(store).reduce(
       (accum: B, key: string) => f(key, store[key], accum),
       initial,
     )
-  }
-
-  public static union<T> (first: Dict<T>, second: Dict<T>): Dict<T> { // curry it
-    const newStore: HashMap<T> = { ...second._store, ...first._store }
-    return new Dict(newStore)
   }
 
   private _store: HashMap<T>
